0495efa4a70e69d4ca68c7ed3dc3e93ea1419fb2
Kensuke Nagae <kyanny@gmail.com>
2017-07-13 04:01:20 +0900

    Types

---
+++ basic-types.ts
@@ -0,0 +1,21 @@
+// https://www.typescriptlang.org/docs/handbook/basic-types.html
+// 
+// Boolean
+let isDone: boolean = false;
+
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o774;
+
+// String
+let color: string = "blue";
+color = 'red';
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}.
+
+I'll be ${ age + 1 } years old next month.`;
+
